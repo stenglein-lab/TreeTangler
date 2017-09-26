565a19ca5215d09757940502d0499c45c189f543
David King <davidcking.inbox@gmail.com>
2017-09-26 12:24:02 -0600

    added getTreeStats

--- CoPhylogenyGraph.js
+++ CoPhylogenyGraph.js
@@ -219,11 +219,41 @@ class CoPhylogenyGraph {
             }
         }
     }
+    getTreeStats(node, data) {
+        if (! data.hasOwnProperty('degree')) {
+            data['degree'] = [0,0,0,0,0,0];
+        }
+        if (node.hasOwnProperty('branchset')) {
+            var count = node['branchset'].length;
+            if (data['degree'].hasOwnProperty(count)) {
+                data['degree'][count]++;
+            }
+            else {
+                data['degree'][count] = 1;
+            }
+            for (var branch in node["branchset"]) {
+                // traverse tree
+                this.getTreeStats(node["branchset"][branch], data);
+            }
+        }
+        else { // leaf node
+            if (data['degree'].hasOwnProperty(0)) {
+                data['degree'][0]++;
+            }
+            else {
+                data['degree'][0] = 1;
+            }
+        }
+    }
     renderTrees(leftTree, rightTree, rescale = true, redraw = true) {
         // json format binary trees, processed from newick style text files by Newick.js.
         this.leftTree = leftTree;
         this.rightTree = rightTree;
         if (redraw) {
+            var leftTreeStats = new Object();
+            this.getTreeStats(this.leftTree, leftTreeStats);
+            console.log("leftTreeStats histogram of node degrees", leftTreeStats['degree'].length);
+            console.dir(leftTreeStats);
             this.addUniqueNodeIds(this.leftTree, true);
             this.addUniqueNodeIds(this.rightTree, false);
             console.dir(this.leftTree);
