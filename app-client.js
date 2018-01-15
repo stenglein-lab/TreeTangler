e5ac7d99897c464f3c4926651c21109d817e1f13
David King <davidcking.inbox@gmail.com>
2018-01-15 12:55:29 -0700

    testbundle.html now works on the browserified CoPhylogeny/d3

--- app-client.js
+++ app-client.js
@@ -1,5 +1,5 @@
 $ = require('jquery');
-cophylogeny = require('lib/CophylogenyGraph');
+cophylogeny = require('./lib/CoPhylogenyGraph');
 
 $(document).ready(function() {
     $('body').append("got here via jquery");
