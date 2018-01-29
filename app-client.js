ebd4208d7e7fca7a84497297264d1b8211f0bfe7
David King <davidcking.inbox@gmail.com>
2018-01-29 15:35:08 -0700

    index now with buttons, slider

--- app-client.js
+++ app-client.js
@@ -1,6 +1,24 @@
 $ = require('jquery');
-cophylogeny = require('./lib/CoPhylogenyGraph');
-
+var bootstrap = require('bootstrap');
+var bootslider = require('bootstrap-slider');
+var cophylogeny = require('./lib/CoPhylogenyGraph');
 $(document).ready(function() {
-    $('body').append("got here via jquery");
+    // URL blobs needed for newick reader
+    var leftURL = null,
+        rightURL = null;
+
+    // hook into the html buttons and inputs
+    var fileButtonLeft = document.getElementById("fileButtonLeft"),
+        fileInputLeft = document.getElementById("fileInputLeft");
+    var fileButtonMiddle = document.getElementById("fileButtonMiddle"),
+        fileInputMiddle = document.getElementById("fileInputMiddle");
+    var fileButtonRight = document.getElementById("fileButtonRight"),
+        fileInputRight = document.getElementById("fileInputRight");
+
+    // hook into slider
+    $('#ex1').slider({
+        formatter: function(value) {
+            $('#currentVertScaleLabel').text(value);
+        }
+    });
 });
--- app.js
+++ app.js
@@ -6,17 +6,26 @@ var express = require('express');
 
 var app = express(); // express instance 
 
-// "middleware" that comes with express server
-app.use(express.static("./public")); // static file server
-// custom middleware
 app.use( function(req,res,next) {
     // req- request,
     // res- response,
     // next- next function to execute
-
+    console.log(`request fresh ${req.fresh}`);
+    console.log(`request hostname ${req.hostname}`);
+    console.log(`request ip ${req.ip}`);
+    console.log(`request path ${req.path}`);
     console.log(`${req.method} request for '${req.url}'`);
 
     next(); // required to actually send a response back
+});
+// "middleware" that comes with express server
+app.use(express.static("./public")); // static file server
+// custom middleware
+// error-handling
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+    next();
 });
 
 app.listen(port);
