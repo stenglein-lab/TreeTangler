79bd20c175d2a7c71de811b4f96ea257ca1cf352
David King <davidcking.inbox@gmail.com>
2018-02-13 08:57:07 -0700

    duplicated commit

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,5 +1,31 @@
-var expect = require("chai").expect;
+var chai = require("chai");
+var expect = chai.expect;
+var deepEqual = chai.deepEqual;
 var treetools = require("../lib/treetools");
+var cophylogeny = require("../lib/cophylogeny");
+var nw = require("../lib/newick");
+
+describe("Module: newick", function(){
+    it("parses a newick-format string", function() {
+        var tree = nw.parse('((B:6.0,(A:5.0,C:3.0)i0:5.0)i1:4.0,D:15.0)simple-tree:10;') // contents of public/simple-tree.newick
+        var expected_tree = { "branchset": [
+            {
+              "name": "i1", "branchset": [
+                { "name": "B", "length": 6 },
+                {
+                  "name": "i0", "branchset": [
+                    { "name": "A", "length": 5 },
+                    { "name": "C", "length": 3 }
+                  ], "length": 5
+                }
+              ], "length": 4
+            }, { "name": "D", "length": 15 }
+          ], "name": "simple-tree", "length": 10
+        };
+        expect(tree).to.deep.equal(expected_tree);
+    });
+    
+});
 
 describe("Module: treetools", function(){
 
